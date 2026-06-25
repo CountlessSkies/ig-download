49e1739ae3efa5e8c521884b9b7550b9bb9442f8
litourgiya <nguyenduydung@outlook.com>
2026-06-25 08:37:54 +0700

    Initial commit

---
+++ inject.js
@@ -0,0 +1,2 @@
+"use strict";(()=>{var script=document.createElement("script");script.setAttribute("type","text/javascript");script.setAttribute("src",chrome.runtime.getURL("xhr.js"));script.onload=()=>{script.remove()};(document.head||document.documentElement).appendChild(script);})();
+//# sourceMappingURL=inject.js.map
